1f5bfd22d92371c3d74a003f4f16e496c305dd05
Bartosz Blimke <bartosz@new-bamboo.co.uk>
2009-04-27 15:05:53 +0200

    Fixed stomp job queue

--- lib/job_queue/adapters/stomp_adapter.rb
+++ lib/job_queue/adapters/stomp_adapter.rb
@@ -6,21 +6,23 @@ class JobQueue::StompAdapter
     password = options[:password]
     host = options[:host] || 'localhost'
     port = options[:port] || 61613
-    @client = Stomp::Client.new(username, password, host, port)
+    @conn = Stomp::Connection.new(username, password, host, port, true)
   end
   
   def put(string)
-    @client.send("job_queue", string)
+    @conn.send("/queue/job_queue", string, :persistent => true)
   end
   
   def subscribe(error_report, &block)
-    @client.subscribe("job_queue") do |job|
-      begin
-        JobQueue.logger.info "Stomp received #{job}"
-        yield job
-      rescue => e
-        error_report.call(job, e)
+      @conn.subscribe("/queue/job_queue")
+      loop do
+        begin
+          job = @conn.receive
+          JobQueue.logger.info "Stomp received #{job}"
+          yield job.body
+        rescue => e
+          error_report.call(job, e)
+        end
       end
-    end
   end
 end
